2a03033063db83f52d2c17545691d64474d58277
TomasM <tomas@slax.org>
2018-11-23 14:33:12 +0000

    Version update v4.0.0

--- xlunch.c
+++ xlunch.c
@@ -2,8 +2,8 @@
 // Licence: GNU GPL v3
 // Authors: Tomas Matejicek <www.slax.org>
 //          Peter Munch-Ellingsen <www.peterme.net>
-const int VERSION_MAJOR = 3; // Major version, changes when breaking backwards compatability
-const int VERSION_MINOR = 3; // Minor version, changes when new functionality is added
+const int VERSION_MAJOR = 4; // Major version, changes when breaking backwards compatability
+const int VERSION_MINOR = 0; // Minor version, changes when new functionality is added
 const int VERSION_PATCH = 0; // Patch version, changes when something is changed without changing deliberate functionality (eg. a bugfix or an optimisation)
 
 #define _GNU_SOURCE
