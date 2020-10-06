56a79ec921efb6b10bad7b6ce029388ad4286e49
Artem Polishchuk <ego.cordatus@gmail.com>
2020-10-06 04:59:28 +0300

    fix: add separate 'genentries.desktop.sh' invoked by .desktop file
    Fix: #109

---
+++ extra/genentries.desktop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xcp=$HOME/.config/xlunch/;xsp=$HOME/.local/share/xlunch;ni=/usr/share/icons/hicolor/48x48/apps/xlunch.png;mkdir -p $xcp && mkdir -p $xsp/svgicons && sh /usr/bin/genentries > $xcp/entries.dsv && ( notify-send -a xlunch -i $ni "Done generating new entries" && mv -f svgicons/* $xsp/svgicons && rm -r svgicons/ && ( cat $xcp/entries.dsv | sed 's|;svgicons\/|;.local/share/xlunch/svgicons/|' > entries2.dsv && mv entries2.dsv $xcp/entries.dsv ) ) || notify-send -a xlunch -i $ni "Error generating new entries"
